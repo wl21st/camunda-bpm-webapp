f3b1e0ce7f5566d1336199cc123484f1398da36f
Valentin Vago <zeropaper@irata.ch>
2015-03-17 11:15:16 +0100

    start again

--- webapp/src/main/resources-plugin/base/app/views/modification/main.js
+++ webapp/src/main/resources-plugin/base/app/views/modification/main.js
@@ -11,6 +11,92 @@ define([
   angular,
   template
 ) {
+  var $ = angular.element;
+
+  var ViewController = [
+    '$scope',
+  function(
+    $scope
+  ) {
+    var instanceData = $scope.processData.newChild($scope);
+
+    $scope.instructions = [];
+    $scope.selectionMode = false;
+
+    $scope.switchSelectionMode = function () {
+      $scope.selectionMode = !$scope.selectionMode;
+    };
+
+    $scope.addInstruction = function (activityId, allInstanceIds) {
+      $scope.instructions.push({
+        operation: 'cancel',
+        activityId: activityId,
+        allInstanceIds: allInstanceIds,
+        activityInstanceIds: allInstanceIds.map(function (inst) {
+          return inst.id ? inst.id : inst;
+        }),
+        target: null,
+        variables: []
+      });
+    };
+
+    $scope.removeInstruction = function (index) {
+      var instructions = [];
+
+      $scope.instructions.forEach(function (instruction, i) {
+        if (index !== i) {
+          instructions.push(instruction);
+        }
+      });
+
+      $scope.instructions = instructions;
+    };
+
+    $scope.changeInstructionOperation = function ($event, $index) {
+      $scope.instructions[$index].operation = $($event.target).val();
+    };
+
+    $scope.toggleInstanceSelections = function (instructionIndex, instanceIndex) {
+      // console.info('toggleInstanceSelections', instructionIndex, instanceIndex);
+    };
+
+    $scope.instructionHasInstance = function (instruction, instanceId) {
+      var ids = instruction.allInstanceIds.map(function (inst) {
+        return inst.id;
+      });
+      return ids.indexOf(instanceId) > -1;
+    };
+
+
+    $scope.operationHasVariables = function (op) {
+      return ['startBefore', 'startAfter', 'move'].indexOf(op) > -1;
+    };
+
+    $scope.operationHasTarget = function (op) {
+      return ['startBefore', 'startAfter', 'move'].indexOf(op) > -1;
+    };
+
+
+    var previousActivityId;
+    instanceData.observe([
+      'filter', 'activityIdToInstancesMap', 'bpmnElements',
+    ], function(
+      filter, activityIdToInstancesMap, bpmnElements
+    ) {
+      var currentActivityId = filter.activityIds[0];
+      $scope.bpmnElements = bpmnElements;
+
+      if (currentActivityId) {
+        if ($scope.selectionMode && previousActivityId !== currentActivityId) {
+          $scope.selectionMode = false;
+          $scope.addInstruction(currentActivityId, activityIdToInstancesMap[currentActivityId]);
+
+          previousActivityId = currentActivityId;
+        }
+      }
+    });
+  }];
+
   return ['ViewsProvider', function (ViewsProvider) {
     ViewsProvider.registerDefaultView('cockpit.processInstance.runtime.tab', {
       id: 'modification-process-instances',
@@ -19,8 +105,7 @@ define([
 
       template: template,
 
-      controller: [function() {
-      }],
+      controller: ViewController,
 
       priority: 20
     });
